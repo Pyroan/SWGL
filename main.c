4f5b48e45e3a887416b6812f4b74be8366c9f57f
Evan Schoenberger <evanjschoenberger@gmail.com>
2018-03-05 14:07:36 -0500

    Initial Commit

---
+++ main.c
@@ -0,0 +1,36 @@
+#include "swgl.h"
+
+void example() {
+/*
+	myVertex v1 = {0.0, 0.0, 0.0, 1.0};
+	myVertex v2 = {10.0, 5.0, 0.0, 1.0};
+	
+	myVertex v3 = {10.0, 5.0, -3.0, 1.0};
+	myVertex v4 = {12.0, 8.0, -4.0, 1.0};
+	myVertex v5 = {6.0, 12.0, -5.0, 1.0};
+
+	myVertex *vector1 = {v3, v4, v5};
+
+	myColor c1 = {1.0, 0.0, 0.0};
+	myColor c2 = {5.0, 0.0, 0.5};
+
+	setView(10, 50, 60, 100);
+
+	// Default OpenGL case
+	setVRC(0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0);
+
+	setProjection(PARALLEL, 0.0, 0.0, 1.0, 30.0, 30.0, 0.0, 10.0);
+
+	myTranslate(2.0, 1.0, -4.0);
+	myScale(2.0, 3.0, 1.0);
+	drawLine(c1, v1, v2);
+	
+	matInit(curMatrix);
+	myRotate(10.0, 1.0, 1.0, -1.0);
+	drawPolyv(c2, vector1, 3);
+*/
+}
+
+int main() {
+	return 0;
+}
---
+++ swgl.h
@@ -0,0 +1,25 @@
+#ifndef SWGL_H_
+#define SWGL_H_
+
+// Data types
+typedef double myVertex[4];
+typedef double myColor[3];
+typedef double myMat[4][4];
+myMat curMatrix;
+
+// Auxiliary functions
+void multMatrix4x4(myMat mat1, myMat mat2);
+myVertex* transformVertex(myMat mat, myVertex vertex);
+void resetVertex(myMat mat, myVertex vertex);
+void initMat (myMat mat);
+
+// Transforms
+void myTranslate(double dx, double dy, double dz);
+void myScale(double sx, double sy, double sz);
+void myRotate(double theta, double x, double y, double z);
+
+// Primitives
+void drawLine(myColor c, myVertex v1, myVertex v2);
+void drawPolyv(myColor c, myVertex *vector, int numVertices);
+
+#endif // SWGL_H_
